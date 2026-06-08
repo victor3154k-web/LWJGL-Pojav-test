ef6dce682697cbe24198bb2b8d89d23905fe9a7e
victor3154k-web <victor3154k@gmail.com>
2026-06-08 15:40:00 -0300

    Update POJAV_NATIVES URL for library downloads

--- ci_build_android.bash
+++ ci_build_android.bash
@@ -75,10 +75,11 @@ if [ "$SKIP_FREETYPE" != "1" ]; then
 fi
 
 # Download libraries
-POJAV_NATIVES="https://github.com/FCL-Team/FoldCraftLauncher/raw/main/FCLauncher/src/main/jniLibs/$NDK_ABI"
+POJAV_NATIVES="https://github.com"
 wget -nc $POJAV_NATIVES/libopenal.so -P $LWJGL_NATIVE/openal
 wget -nc "https://github.com/AngelAuraMC/shaderc/releases/latest/download/libshaderc-$NDK_ABI.zip"
 unzip -o libshaderc-$NDK_ABI.zip -d $LWJGL_NATIVE/shaderc
+
 
 # HACK: Skip compiling and running the generator to save time and keep LWJGLX functions
 mkdir -p bin/classes/{generator,templates/META-INF}
